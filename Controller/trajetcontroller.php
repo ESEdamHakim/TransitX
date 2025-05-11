9df697bd39edb3ab78cbe4081fb149eb664ad61e
ESEdamHakim <Edam.Hakim@esprit.tn>
2025-05-11 03:31:09 +0100

    .

--- Controller/trajetcontroller.php
+++ Controller/trajetcontroller.php
@@ -1,6 +1,6 @@
 <?php
-require_once(__DIR__ . "/../config.php");
-require_once(__DIR__ . "/../Model/trajetmodel.php");
+require_once __DIR__ . "/../config.php";
+require_once __DIR__ . "/../Model/trajetmodel.php";
 
 class TrajetController
 {
