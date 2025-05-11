44a001d896ee1fc7d513e3326e0c0412833190c9
ESEdamHakim <Edam.Hakim@esprit.tn>
2025-05-11 04:43:59 +0100

    .

--- View/BackOffice/blog/deletearticle.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/../../../Controller/ArticleC.php';
-    $articc = new ArticleC();
-        $articc->deletearticle($_GET['id_article']);
-        
-        header("Location: crud.php");
-       
-?>
